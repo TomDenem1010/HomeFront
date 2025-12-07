5b600b37e45187361d664b15bcbff3b26164a783
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2025-12-07 18:45:32 +0100

    NEW: kereső opcio

--- src/main/java/com/home/front/controller/VideoController.java
+++ src/main/java/com/home/front/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.home.front.controller;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -15,6 +16,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.home.common.dto.HomeHeader;
 import com.home.common.dto.HomeRequest;
+import com.home.common.video.dto.ActorDto;
+import com.home.common.video.dto.FolderDto;
+import com.home.common.video.dto.VideoDto;
+import com.home.common.video.dto.findActors.FindActorsRequest;
+import com.home.common.video.dto.findByActor.FindByActorRequest;
+import com.home.common.video.dto.findByActor.FindByActorRequestWrapper;
+import com.home.common.video.dto.findByFolder.FindByFolderRequest;
+import com.home.common.video.dto.findByFolder.FindByFolderRequestWrapper;
+import com.home.common.video.dto.findFolders.FindFoldersRequest;
 import com.home.common.video.dto.findVideos.FindVideosRequest;
 import com.home.front.client.VideoClient;
 
@@ -27,15 +37,30 @@ public class VideoController {
 
     private final VideoClient videoClient;
 
-    @GetMapping("/videos/findVideos")
-    public ModelAndView findVideos() {
+    @GetMapping("/videos")
+    public ModelAndView videos() {
         Map<String, Object> model = new HashMap<>();
-        model.put(
-                "videos",
-                videoClient.findVideos((FindVideosRequest) setHeader(new FindVideosRequest()))
-                        .getResponse()
-                        .getVideos());
+        model.put("videos", findVideos());
+        model.put("folders", findFolders());
+        model.put("actors", findActors());
+        return new ModelAndView("videos", model);
+    }
 
+    @GetMapping("/videos/findByFolder")
+    public ModelAndView findVideosByFolder(String folder) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("videos", findByFolder(folder));
+        model.put("folders", findFolders());
+        model.put("actors", findActors());
+        return new ModelAndView("videos", model);
+    }
+
+    @GetMapping("/videos/findByActor")
+    public ModelAndView findVideosByActor(String actor) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("videos", findByActor(actor));
+        model.put("folders", findFolders());
+        model.put("actors", findActors());
         return new ModelAndView("videos", model);
     }
 
@@ -48,6 +73,40 @@ public class VideoController {
                         .substring(request.getRequestURI().indexOf("/stream/") + "/stream/".length()),
                         StandardCharsets.UTF_8),
                 rangeHeader);
+    }
+
+    private List<VideoDto> findVideos() {
+        return videoClient.findVideos((FindVideosRequest) setHeader(new FindVideosRequest()))
+                .getResponse()
+                .getVideos();
+    }
+
+    private List<VideoDto> findByFolder(String folder) {
+        FindByFolderRequestWrapper wrapper = new FindByFolderRequestWrapper();
+        wrapper.setRequest(new FindByFolderRequest(folder));
+        return videoClient.findByFolder((FindByFolderRequestWrapper) setHeader(wrapper))
+                .getResponse()
+                .getVideos();
+    }
+
+    private List<VideoDto> findByActor(String actor) {
+        FindByActorRequestWrapper wrapper = new FindByActorRequestWrapper();
+        wrapper.setRequest(new FindByActorRequest(actor));
+        return videoClient.findByActor((FindByActorRequestWrapper) setHeader(wrapper))
+                .getResponse()
+                .getVideos();
+    }
+
+    private List<FolderDto> findFolders() {
+        return videoClient.findFolders((FindFoldersRequest) setHeader(new FindFoldersRequest()))
+                .getResponse()
+                .getFolders();
+    }
+
+    private List<ActorDto> findActors() {
+        return videoClient.findActors((FindActorsRequest) setHeader(new FindActorsRequest()))
+                .getResponse()
+                .getActors();
     }
 
     private HomeRequest<?> setHeader(HomeRequest<?> homeRequest) {
